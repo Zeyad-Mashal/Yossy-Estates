61935d3a0592fe46b40544a34c5625d9ebeb3bbf
Mashal <zyadomar112@gmail.com>
2025-01-06 15:09:32 +0200

    first commit

---
+++ JS/app.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    const menu = document.querySelector('.menu');
+    menu.classList.toggle('open');
+}
+function contact_done() {
+    const contact_done = document.querySelector('.contact_submit');
+    contact_done.style.display = "block";
+}
